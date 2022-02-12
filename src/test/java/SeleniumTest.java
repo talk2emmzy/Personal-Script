addab497bd88d5ee37b39b9a8ef9d6beaf9dbe0c
Okoye Emmanuel <Okoye@omnibiz.com>
2022-02-13 00:21:02 +0100

    first test commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    @Test
+    public void firstTest()
+    {
+        //Telling the system where to find chromedriver. On Windows we can add .exe
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        //Instantiate the driver
+        WebDriver driver = new ChromeDriver();
+        //Goto the url
+        driver.get("https://www.saucedemo.com/");
+        //Finding username elements
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        //Finding password elements
+        driver.findElement(By.id("password")).sendKeys("password");
+        //Finding Login elements
+        driver.findElement(By.id("login-button")).click();
+        driver.quit();
+    }
+}
